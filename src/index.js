b405986553bc4fd2ac12f0d0be43cde7c1728143
PitLion <pitmarya@gmail.com>
2023-01-08 23:44:42 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ module.exports = function reverse (n) {
     let strOfNumberArray =  strOfNumber.split('');
     let reverseArray = strOfNumberArray.reverse();
     let reverseStrOfNumber = reverseArray.join('');
+    
     return Number(reverseStrOfNumber);
   }
